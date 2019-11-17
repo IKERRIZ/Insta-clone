32bec999849d0aa598ed310d640784d0babad445
IKERRIZ <okothfaith94@gmail.com>
2019-11-17 03:44:28 +0300

    Add url pattern for the registratio template

--- instagram/urls.py
+++ instagram/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    
+
     url(r'^admin/', admin.site.urls),
-    url(r'', include('insta.urls'))
+    url(r'', include('insta.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+
 ]

1f0896fb4108249c3b3b2fbc13b06878411e977f
IKERRIZ <okothfaith94@gmail.com>
2019-11-17 15:59:47 +0300

    Add a class for image form

--- insta/forms.py
+++ insta/forms.py
@@ -6,3 +6,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['belongs_to'] 
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['likes', 'profile']

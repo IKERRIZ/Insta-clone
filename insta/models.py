6a3fb37c7327693f5b613c65eb6e7441bbc03184
IKERRIZ <okothfaith94@gmail.com>
2019-11-18 07:57:50 +0300

    Add class likes to the models

--- insta/models.py
+++ insta/models.py
@@ -33,9 +33,9 @@ class Image(models.Model):
     pic=ImageField(manual_crop='1080x800')
     name= models.CharField(max_length=55)
     caption = models.TextField(blank=True)
-    likes=models.BooleanField(default=False)
     profile= models.ForeignKey(User, on_delete=models.CASCADE,default=False)
-    comments = models.ManyToManyField('Profile')
+    profile_details = models.ForeignKey(Profile)
+
 
     def __str__(self):
         return str(self.name)
@@ -69,3 +69,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.comment)
+
+class Likes(models.Model):
+    liker=models.ForeignKey(User)
+    image =models.ForeignKey(Image)
+
+
+    def __str__(self):
+        return self.image

b165d1d6432fc6eb5f4d0d83e9723166475c8830
IKERRIZ <okothfaith94@gmail.com>
2019-11-18 12:20:32 +0300

    update readme

--- insta/urls.py
+++ insta/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,6 +9,10 @@ urlpatterns = [
     url(r'^newprofile/',views.profile,name ='profile'),
     url(r'^showprofile/(?P<id>\d+)',views.display_profile,name = 'showprofile'),
     url(r'^image/$', views.add_image, name='upload_image'),
-    url(r'^ search/',views.search, name='search'),
-
+    url(r'^search/',views.search, name='search'),
+    url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+    url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
+    url(r'^follow/(?P<user_id>\d+)', views.follow, name='follow'),
 ] 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
